4f312050664f9c952242a436fee35d7cab6be67d
Link <fahriicat@Links-MacBook-Pro.local>
2022-11-16 21:33:30 -0500

    fix linter

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import {
-  createHashRouter,
-  RouterProvider,
+    createHashRouter,
+    RouterProvider,
 } from "react-router-dom";
 import "./index.css";
 import Home from "./Home"
 import Commissions from "./Commissions"
 
 const router = createHashRouter([
-  {
-    path: "/",
-    element: <Home></Home>
-  },
-  {
-    path: "/commissions",
-    element: <Commissions></Commissions>
-  }
+    {
+        path: "/",
+        element: <Home></Home>
+    },
+    {
+        path: "/commissions",
+        element: <Commissions></Commissions>
+    }
 ], {basename: "/"});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 );

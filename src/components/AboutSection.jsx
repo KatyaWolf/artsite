ba5975fab27bd17f1a3a84e87723c334dc924fae
Link <fahriicat@Links-MacBook-Pro.local>
2022-11-16 21:09:32 -0500

    added alt text

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -3,10 +3,10 @@ import React from "react"
 const AboutSection = () => {
     return (
         <div className="text-center grid grid-cols-2 gap-4">
-            <img src={process.env.PUBLIC_URL+"/img/portrait.jpeg"} className="w-96 pt-8" />
+            <img src={process.env.PUBLIC_URL+"/img/portrait7.jpeg"} alt="its a'me, mario" className="w-96 pt-8" />
             <div>
                 <h3 className="text-4xl text-fuchsia-800 font-serif overline decoration-purple-400 pt-28">﹝ FEATHERHYTHM ﹞</h3>
-            <p>Lonk is a 25 year old Russian artist living in Indiana with her three cats.
+            <p>Link is a 25 year old Russian artist living in Indiana with her three cats.
                 She is a MarComm communications specialist by day and illustrator by night.
                 She loves a good iced coffee, weighted blankets, and naps. </p>
             </div>

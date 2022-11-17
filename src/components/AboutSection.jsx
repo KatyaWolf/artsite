73eb6fe5e4c0addde3c72d92348778c3acf226f1
Link <fahriicat@Links-MacBook-Pro.local>
2022-11-16 21:24:19 -0500

    add eslint

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -6,7 +6,7 @@ const AboutSection = () => {
             <img src={process.env.PUBLIC_URL+"/img/portrait7.jpeg"} alt="its a'me, mario" className="w-96 pt-8" />
             <div>
                 <h3 className="text-4xl text-fuchsia-800 font-serif overline decoration-purple-400 pt-28">﹝ FEATHERHYTHM ﹞</h3>
-            <p>Link is a 25 year old Russian artist living in Indiana with her three cats.
+                <p>Link is a 25 year old Russian artist living in Indiana with her three cats.
                 She is a MarComm communications specialist by day and illustrator by night.
                 She loves a good iced coffee, weighted blankets, and naps. </p>
             </div>
